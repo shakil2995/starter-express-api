fd033e55089f835704736a16134298bc02ecc66d
Shakil <shakil2995@gmail.com>
2022-12-27 03:28:41 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,89 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+//jshint esversion:6
+const express = require('express');
+const app = express();
+// GeolocationCoordinates = require('./models/geolocationCoordinates');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+const cors = require('cors');
+app.use(cors());
+const nodemon = require('nodemon');
+var latLngIub = {
+	"results": [
+		{
+			"busNo": 0,
+			"institute": "IUB",
+			"location": {
+				"coordinates": {
+					"latitude": "23.864191",
+					"longitude": "90.381035"
+				},
+			},
+		},
+		{
+			"busNo": 1,
+			"institute": "IUB",
+			"location": {
+				"coordinates": {
+					"latitude": "23.854191",
+					"longitude": "90.405035"
+				},
+			},
+		},
+		{
+			"busNo": 2,
+			"institute": "IUB",
+			"location": {
+				"coordinates": {
+					"latitude": "23.844191",
+					"longitude": "90.371035"
+				},
+			},
+		},
+
+	],
+};
+var latLngNsu = {
+	"results": [
+		{
+			"busNo": 0,
+			"institute": "NSU",
+			"location": {
+				"coordinates": {
+					"latitude": "23.864191",
+					"longitude": "90.381035"
+				},
+			},
+		},
+		{
+			"busNo": 1,
+			"institute": "NSU",
+			"location": {
+				"coordinates": {
+					"latitude": "23.854191",
+					"longitude": "90.405035"
+				},
+			},
+		},
+
+	],
+};
+
+app.get("/", function (req, res) {
+	res.status(200);
+	res.send("<h1>Hello world</h1>");
+});
+app.get("/coords/iub" || "/coords/IUB", function (req, res) {
+	res.status(200).send(latLngIub);
+});
+app.get("/coords/nsu" || "/coords/NSU", function (req, res) {
+	res.status(200).send(latLngNsu);
+});
+
+app.post("/coords/iub/busno", function (req, res) {
+	latLng.results[req.query.busno].location.coordinates.latitude = req.query.lat;
+	latLng.results[req.query.busno].location.coordinates.longitude = req.query.lng;
+	res.send(latLng);
+});
+app.listen(process.env.PORT || 3000, function (req, res) {
+	console.log("server Working");
 })
-app.listen(process.env.PORT || 3000)
